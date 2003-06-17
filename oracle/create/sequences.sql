3f04589f6fb10cbde028eb54cfb152ad3fa9d081
(no author) <(no author)>
2003-06-17 17:36:46 +0000

    This commit was manufactured by cvs2svn to create tag 'LOCAL_PARTNERS_BETA'.
    git-svn-id: http://svn.nci.nih.gov/svn/planet3/tags/LOCAL_PARTNERS_BETA@80 41d1f371-2c4a-214d-948b-9ca61c02e737

---
+++ oracle/create/sequences.sql
@@ -0,0 +1,4 @@
+/* $Id: sequences.sql,v 1.1 2003-05-19 20:17:27 juranj Exp $ */
+
+CREATE SEQUENCE cc_partners_seq;
+CREATE SEQUENCE cc_partner_contacts_seq;
---
+++ oracle/proc/body/planet.sql
@@ -0,0 +1,49 @@
+CREATE OR REPLACE PACKAGE BODY planet_pkg
+AS
+    /* $Id: planet.sql,v 1.1 2003-05-22 19:56:41 juranj Exp $ */
+    PROCEDURE GetStateList(p_cursor OUT refcursor)
+    IS
+    BEGIN
+        OPEN p_cursor FOR
+            SELECT type, abbreviation, name
+            FROM cc_partner_states
+            ORDER BY type, name;
+    END GetStateList;
+
+    PROCEDURE GetPartners(p_topic IN cc_partners.cctopic%TYPE, p_cursor OUT refcursor)
+    IS
+    BEGIN
+        OPEN p_cursor FOR
+            SELECT region state_abbreviation, ps.name state_name,
+                p.abbreviation partner_abbreviation, p.name partner_name,
+                pc.type, pc.name contact_name,
+                degree, title, org1, org2, orgurl, address1, address2, city,
+                pc.state address_state, zip, phone, fax, cell, email
+            FROM cc_partners p, cc_partner_contacts pc, cc_partner_states ps
+            WHERE p.id = pc.partner_id
+                AND pc.region = ps.abbreviation
+                AND pc.type <> 'N'
+                AND (cctopic IS NULL OR cctopic = p_topic)
+            ORDER BY ps.name, p.abbreviation, pc.type, contact;
+    END GetPartners;
+
+    PROCEDURE GetPartners(p_topic IN cc_partners.cctopic%TYPE,
+                         p_state cc_partner_contacts.region%TYPE, p_cursor OUT refcursor)
+    IS
+    BEGIN
+        OPEN p_cursor FOR
+            SELECT region state_abbreviation, ps.name state_name,
+                p.abbreviation partner_abbreviation, p.name partner_name,
+                pc.type, pc.name contact_name,
+                degree, title, org1, org2, orgurl, address1, address2, city,
+                pc.state address_state, zip, phone, fax, cell, email
+            FROM cc_partners p, cc_partner_contacts pc, cc_partner_states ps
+            WHERE p.id = pc.partner_id
+                AND pc.region = ps.abbreviation
+                AND pc.type <> 'N'
+                AND (cctopic IS NULL OR cctopic = p_topic)
+                AND region = p_state
+            ORDER BY ps.name, p.abbreviation, pc.type, contact;
+    END GetPartners;
+END planet_pkg;
+/
