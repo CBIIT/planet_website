b2f8ad2011e9de532437cf920ca675854bb40aba
juranj <juranj>
2003-07-11 13:55:18 +0000

    Incorporate additional change from Qi.
    git-svn-id: http://svn.nci.nih.gov/svn/planet3/trunk@99 41d1f371-2c4a-214d-948b-9ca61c02e737

--- wwwroot/popwindow.js
+++ wwwroot/popwindow.js
@@ -22,7 +22,7 @@ if (parseInt(navigator.appVersion) >= 4) { win.window.focus(); }
 function popWindowBig(mypage, myname, w, h, scroll) {
 var winl = (screen.width - w) / 2;
 var wint = (screen.height - h) / 2;
-winprops = 'height='+h+',width='+w+',top='+wint+',left='+winl+',scrollbars='+scroll+', resizable'
+winprops = 'height='+h+',width='+w+',top='+wint+',left='+winl+',scrollbars='+scroll+', resizable=no'
 win = window.open(mypage, myname, winprops)
 //if (parseInt(navigator.appVersion) >= 4) { win.window.focus(); }
 }
