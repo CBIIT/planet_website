489236105c484b254a6cce35c9862fe269b9e67d
(no author) <(no author)>
2003-07-28 19:18:09 +0000

    This commit was manufactured by cvs2svn to create tag 'DEPLOY_20030728-2'.
    git-svn-id: http://svn.nci.nih.gov/svn/planet3/tags/DEPLOY_20030728-2@104 41d1f371-2c4a-214d-948b-9ca61c02e737

--- wwwroot/popwindow.js
+++ wwwroot/popwindow.js
@@ -5,12 +5,26 @@ function popWindow(theurl)
 	window.open(theurl,'','toolbar=yes,location=yes,scrollbars=yes,resizable=yes,width=725,height=400');
 }
 
-function popWindowBig(theurl)
-{
-	theurl = theurl.replace(/\s/g, '+');
-	window.open(theurl,'','toolbar=yes,location=yes,scrollbars=yes,resizable=no,width=900,height=650');
+//function popWindowBig(theurl)
+//{
+	//theurl = theurl.replace(/\s/g, '+');
+	//window.open(theurl,'','toolbar=yes,location=yes,scrollbars=yes,resizable=no,width=900,height=650,left=62,top=100');
+//}
+
+//function popWindow(mypage, myname, w, h, scroll) {
+//var winl = (screen.width - w) / 2;
+//var wint = (screen.height - h) / 2;
+//winprops = 'height='+h+',width='+w+',top='+wint+',left='+winl+',scrollbars='+scroll+',resizable'
+//win = window.open(mypage, myname, winprops)
+//if (parseInt(navigator.appVersion) >= 4) { win.window.focus(); }
+//}
+
+function popWindowBig(mypage, myname, w, h, scroll) {
+var winl = (screen.width - w) / 2;
+var wint = (screen.height - h) / 2;
+winprops = 'height='+h+',width='+w+',top='+wint+',left='+winl+',scrollbars='+scroll+''
+win = window.open(mypage, myname, winprops)
+//if (parseInt(navigator.appVersion) >= 4) { win.window.focus(); }
 }
-
-
 
 //* <a href="http://www.cdc.gov/cancer/ncccp/guidelines/index.htm" onclick="javascript:popWindow('http://www.cdc.gov/cancer/ncccp/guidelines/index.htm'); return false;"> *//
