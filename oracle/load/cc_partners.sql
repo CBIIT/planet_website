ca69f7cf44521769685743ce3ece0f689df26344
(no author) <(no author)>
2003-08-12 17:30:03 +0000

    This commit was manufactured by cvs2svn to create tag 'ATLAS_TEST3'.
    git-svn-id: http://svn.nci.nih.gov/svn/planet3/tags/ATLAS_TEST3@111 41d1f371-2c4a-214d-948b-9ca61c02e737

--- oracle/load/cc_partners.sql
+++
@@ -1,7 +0,0 @@
-/* $Id: cc_partners.sql,v 1.1 2003-05-19 20:33:15 juranj Exp $ */
-
-INSERT INTO cc_partners (abbreviation, cctopic, name) VALUES ('ACS',NULL,'American Cancer Society');
-INSERT INTO cc_partners (abbreviation, cctopic, name) VALUES ('CDC','C','CDC Comprehensive Cancer Control Network');
-INSERT INTO cc_partners (abbreviation, cctopic, name) VALUES ('CIS',NULL,'NCI Cancer Information Service');
-INSERT INTO cc_partners (abbreviation, cctopic, name) VALUES ('CDC','T','CDC Tobacco Control Network');
-INSERT INTO cc_partners (abbreviation, cctopic, name) VALUES ('CDC','P','CDC Physical Activity Network');
