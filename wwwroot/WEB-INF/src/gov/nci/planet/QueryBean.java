4126a4bf7b0a34e556fd38b36260c106cb569bfd
francisn <francisn>
2004-04-21 19:17:08 +0000

    CR#13870 -- count researchers by topic
    git-svn-id: http://svn.nci.nih.gov/svn/planet3/trunk@379 41d1f371-2c4a-214d-948b-9ca61c02e737

--- wwwroot/WEB-INF/src/gov/nci/planet/QueryBean.java
+++ wwwroot/WEB-INF/src/gov/nci/planet/QueryBean.java
@@ -12,7 +12,7 @@ public class QueryBean
     {
 	conn = ConnPoolBean.getConnection();
     }
-    
+
     public void close() throws SQLException
     {
         conn.close();
@@ -25,7 +25,7 @@ public class QueryBean
 	stmt.execute();
 	return ((OracleCallableStatement)stmt).getCursor(1);
     }
-    
+
     public ResultSet getPartners(String topic) throws SQLException
     {
         CallableStatement stmt = conn.prepareCall("{call dccps.planet_pkg.GetPartners(?, ?)}");
@@ -34,7 +34,7 @@ public class QueryBean
 	stmt.execute();
 	return ((OracleCallableStatement)stmt).getCursor(2);
     }
-    
+
     public ResultSet getPartners(String topic, String stateAbbreviation) throws SQLException
     {
         CallableStatement stmt = conn.prepareCall("{call dccps.planet_pkg.GetPartners(?, ?, ?)}");
@@ -44,16 +44,80 @@ public class QueryBean
 	stmt.execute();
 	return ((OracleCallableStatement)stmt).getCursor(3);
     }
-    
+
+    public ResultSet getResearchers(Integer topic) throws SQLException
+    {
+        CallableStatement stmt = conn.prepareCall("{call dccps.planet_pkg.GetResearchers(?, ?)}");
+        stmt.setInt(1, topic.intValue());
+				stmt.registerOutParameter(2, OracleTypes.CURSOR);
+				stmt.execute();
+				return ((OracleCallableStatement)stmt).getCursor(2);
+    }
+
+    public ResultSet getResearchers(Integer topic, String stateAbbreviation) throws SQLException
+    {
+        CallableStatement stmt = conn.prepareCall("{call dccps.planet_pkg.GetResearchers(?, ?, ?)}");
+        stmt.setInt(1, topic.intValue());
+        stmt.setString(2, stateAbbreviation);
+				stmt.registerOutParameter(3, OracleTypes.CURSOR);
+				stmt.execute();
+				return ((OracleCallableStatement)stmt).getCursor(3);
+    }
+
     public String getTopicDescription(String topic) throws SQLException
     {
-        CallableStatement stmt = conn.prepareCall("{? = call dccps.planet_pkg.getTopicDescription(?)}");
+        CallableStatement stmt = conn.prepareCall("{? = call dccps.planet_pkg.GetTopicDescription(?)}");
         stmt.registerOutParameter(1, Types.VARCHAR);
         stmt.setString(2, topic);
         stmt.execute();
         return stmt.getString(1);
     }
-    
+
+    public String getTopicDescription(Integer topic) throws SQLException
+    {
+        CallableStatement stmt = conn.prepareCall("{? = call dccps.planet_pkg.GetTopicDescription(?)}");
+        stmt.registerOutParameter(1, Types.VARCHAR);
+        stmt.setInt(2, topic.intValue());
+        stmt.execute();
+        return stmt.getString(1);
+    }
+
+    public Integer getTopicsResearcherCount() throws SQLException
+    {
+	    	CallableStatement stmt = conn.prepareCall("{? = call dccps.planet_pkg.GetTopicsResearcherCount(?)}");
+	    	stmt.registerOutParameter(1, OracleTypes.CURSOR);
+	    	stmt.execute();
+	    	return ((OracleCallableStatement)stmt).getCursor(1);
+	  }
+
+    public Integer getTopicsResearcherCount(String stateAbbreviation) throws SQLException
+    {
+	    	CallableStatement stmt = conn.prepareCall("{? = call dccps.planet_pkg.GetTopicsResearcherCount(?,?)}");
+	    	stmt.registerOutParameter(1, OracleTypes.CURSOR);
+	    	stmt.setString(2, stateAbbreviation);
+	    	stmt.execute();
+	    	return ((OracleCallableStatement)stmt).getCursor(1);
+	  }
+
+    public Integer getTopicResearcherCount(Integer topic) throws SQLException
+    {
+	    	CallableStatement stmt = conn.prepareCall("{? = call dccps.planet_pkg.GetTopicResearcherCount(?)}");
+	    	stmt.registerOutParameter(1, Types.INTEGER);
+	    	stmt.setInt(2, topic.intValue());
+	    	stmt.execute();
+	    	return stmt.getInt(1);
+	  }
+
+    public Integer getTopicResearcherCount(Integer topic, String stateAbbreviation) throws SQLException
+    {
+	    	CallableStatement stmt = conn.prepareCall("{? = call dccps.planet_pkg.GetTopicResearcherCount(?,?)}");
+	    	stmt.registerOutParameter(1, Types.INTEGER);
+	    	stmt.setInt(2, topic.intValue());
+	    	stmt.setString(3, stateAbbreviation);
+	    	stmt.execute();
+	    	return stmt.getInt(1);
+	  }
+
     public void saveFeedback(String feedbackText, String email, String phone) throws SQLException
     {
         CallableStatement stmt = conn.prepareCall("{call dccps.products_order_pkg.save_feedback(?, ?, ?, ?)}");
