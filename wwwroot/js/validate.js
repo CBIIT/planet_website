169f71fc9b2fe65ae4d4570433eab230de658e12
breedlor <rel.breedlove@icf.com>
2015-06-12 17:10:53 +0000

    trying to add js folder
    git-svn-id: http://svn.nci.nih.gov/svn/planet3/trunk@2255 41d1f371-2c4a-214d-948b-9ca61c02e737

---
+++ wwwroot/js/validate.js
@@ -0,0 +1,44 @@
+<!--
+function validateForm(fieldNameToValidate)
+{
+	if (document.form1[fieldNameToValidate].value.length > 0) {
+	
+   		var sqlInjectionPattern = new RegExp("\\w*((\\%27)|(\\'))((\\%6F)|o|(\\%4F))((\\%72)|r|(\\%52))");
+   		var crossSiteScriptingPattern = new RegExp("((\\%3C)|<)(.|\\n)*?((\\%3E)|>)");
+   		var phonePattern = new RegExp("\\([0-9]{3}\\)\\s?[0-9]{3}(-|\\s)?[0-9]{4}(\\s(x\\d+)?){0,1}$|^[0-9]{3}-?[0-9]{3}-?[0-9]{4}(\\s(x\\d+)?){0,1}$");
+   		var emailPattern = new RegExp("[\\w-\\.]+@(?!acme-hack)([\\w-]+\\.)+[\\w-]{2,4}$");
+		var spamLinkPattern = "^(http://)";
+   		var spamConsonantsPattern = new RegExp("[^aeiouyAEIOUY0-9\\n\\ \\/\\:\\.\\,\\-]{7,}");
+		var spamPrescriptionPattern = new RegExp("(?=.*\\bcheap\\b)(?=.*\\bprescription\\b)");
+
+   		if ( (document.form1[fieldNameToValidate].value.match(sqlInjectionPattern)) ||
+			 (document.form1[fieldNameToValidate].value.match(crossSiteScriptingPattern)) ) {
+			window.alert("Please remove any special characters.");
+			return false;
+		} else if (document.form1[fieldNameToValidate].value.match(spamLinkPattern) ||
+			document.form1[fieldNameToValidate].value.match(spamConsonantsPattern) ||
+			document.form1[fieldNameToValidate].value.match(spamPrescriptionPattern) )
+		{
+			window.alert("Please check spelling, expand any long acronyms or reword your input.");
+			return false;
+		}		
+   	} else {
+    	window.alert("Please enter your comments.");
+		return false;
+	}
+
+	if (document.form1.email.value.length > 0) {
+		  	if (!document.form1.email.value.match(emailPattern)) {
+		    	window.alert("Please enter a valid email address.");
+				return false;
+			}
+	}
+	
+	if (document.form1.phone.value.length > 0) {
+		  	if (!document.form1.phone.value.match(phonePattern)) {
+		    	window.alert("Please enter a valid phone number (e.g., 8005551212, (800)555-1212 x1234, 800-555-1212 x123).");
+				return false;
+			}
+	}
+}
+//-->
